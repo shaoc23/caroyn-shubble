cc800741bd85b4b1435cace4ca2b263706a90d37
shaoc23 <shaoc@rpi.edu>
2025-10-25 23:31:26 -0400

    dropdown updates

--- client/src/components/Dropdown/Dropdown.jsx
+++ client/src/components/Dropdown/Dropdown.jsx
@@ -3,29 +3,32 @@ import DropdownButton from "../DropdownButton/DropdownButton";
 import DropdownContent from "../DropdownContent/DropdownContent";
 import "./Dropdown.css";
 
+const Dropdown = ({ buttonText, content }) => {
+  const [open, setOpen] = useState(false);
+  const toggleDropdown = () => setOpen(!open);
+  const closeDropdown = () => setOpen(false);
 
+  return (
+    <div className="dropdown">
+      <DropdownButton toggle={toggleDropdown} open={open}>
+        {buttonText}
+      </DropdownButton>
 
-const Dropdown = ({buttonText,content}) => {
-
-    const [open, setOpen] = useState(false);
-    // const toggleDropdown = () => {
-    //     setOpen((open) => !open)
-    // }
-
-    const [dropdown, setDropdown] = useState(false);
-
-    return (
-        <div className="dropdown">
-        <DropdownButton toggle={toggleDropdown} open={open}>
-         {buttonText}
-        </DropdownButton>
-        <DropdownContent open={open}>
-            {content}
-        </DropdownContent>
-
-        
+      <DropdownContent open={open}>
+        {/* ✅ Wrap the content with a click handler */}
+        <div
+          onClick={(e) => {
+            // only close when clicking a link, not when toggling dropdown
+            if (e.target.tagName === "A") {
+              closeDropdown();
+            }
+          }}
+        >
+          {content}
+        </div>
+      </DropdownContent>
     </div>
-    )
+  );
 };
 
 export default Dropdown;
